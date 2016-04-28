dd4c6f7290d7c783582adda1e371932932cba462
lef <le.florez602@uniandes.edu.co>
2016-04-28 12:41:48 -0500

    Angular correcion y estandarizacion de controller.js

--- MHack_Project/ScripToForm/public/angular_app/saldobodega/saldobodega.controller.js
+++ MHack_Project/ScripToForm/public/angular_app/saldobodega/saldobodega.controller.js
@@ -14,16 +14,16 @@
     });
 
     //refs
-    vm.data.selectedBodegaOption = {};
+    vm.data.selectedIdBodegaOption = {};
 
     invData.getBodegas(function(data){
-      vm.data.bodegas = data;
+      vm.data.idBodegas = data;
     });
 
-    vm.data.selectedProductoOption = {};
+    vm.data.selectedIdProductoOption = {};
 
     invData.getProductos(function(data){
-      vm.data.productos = data;
+      vm.data.idProductos = data;
     });
 
     vm.data.create = true;
@@ -31,18 +31,18 @@
     vm.addSaldoBodega = function(){
       vm.data.create = true;
       vm.data.detalle = {};
-      vm.data.selectedBodegaOption = {};
-      vm.data.selectedProductoOption = {};
+      vm.data.selectedIdBodegaOption = {};
+      vm.data.selectedIdProductoOption = {};
     };
 
     vm.editSaldoBodega = function(detalle){
       vm.data.create = false;
       vm.data.detalle = angular.copy(detalle);
-      vm.data.selectedBodegaOption = _.find(vm.data.bodegas, function(data){
+      vm.data.selectedIdBodegaOption = _.find(vm.data.idBodegas, function(data){
         return data.id === vm.data.detalle.idBodega;
       });
 
-      vm.data.selectedProductoOption = _.find(vm.data.productos, function(data){
+      vm.data.selectedIdProductoOption = _.find(vm.data.idProductos, function(data){
         return data.id === vm.data.detalle.idProducto;
       });
 
@@ -51,23 +51,23 @@
     vm.crearSaldoBodega = function(){
       vm.data.create = false;
       var detalle = angular.copy(vm.data.detalle);
-      detalle.idBodega = vm.data.selectedBodegaOption.id;
-      detalle.idProducto = vm.data.selectedProductoOption.id;
+      detalle.idBodega = vm.data.selectedIdBodegaOption.id;
+      detalle.idProducto = vm.data.selectedIdProductoOption.id;
 
       invData.addSaldoBodega(detalle, function(detalle){
           vm.data.lista.push(detalle);
       });
 
       vm.data.detalle = {};
-      vm.data.selectedBodegaOption = {};
-      vm.data.selectedProductoOption = {};
+      vm.data.selectedIdBodegaOption = {};
+      vm.data.selectedIdProductoOption = {};
     };
 
     vm.updateSaldoBodega = function(){
       vm.data.create = false;
       var detalle = angular.copy(vm.data.detalle);
-      detalle.idBodega = vm.data.selectedBodegaOption.id;
-      detalle.idProducto = vm.data.selectedProductoOption.id;
+      detalle.idBodega = vm.data.selectedIdBodegaOption.id;
+      detalle.idProducto = vm.data.selectedIdProductoOption.id;
 
       invData.updateSaldoBodega(detalle, function(detalle){
 
@@ -80,8 +80,8 @@
       });
 
       vm.data.detalle = {};
-      vm.data.selectedBodegaOption = {};
-      vm.data.selectedProductoOption = {};
+      vm.data.selectedIdBodegaOption = {};
+      vm.data.selectedIdProductoOption = {};
     };
 
     vm.deleteSaldoBodega = function(id){
